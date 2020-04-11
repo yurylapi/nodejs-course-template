67bcf99c1ce37aa5f1afdeccece4e99a581ade09
yurylapi <yuri.lapitsky@gmail.com>
2020-04-11 22:51:35 +0200

    Merge branch 'master' of https://github.com/rolling-scopes-school/nodejs-course-template
    # Conflicts:
    #	TASKS.md
    #	package-lock.json
    #	package.json
    #	src/app.js
    #	src/resources/users/user.memory.repository.js
    #	src/resources/users/user.model.js
    #	src/resources/users/user.router.js
    #	src/resources/users/user.service.js
    #	test/e2e/test/boards.test.js
    #	test/e2e/test/tasks.test.js
    #	test/e2e/test/users.test.js

--- src/server.js
+++ src/server.js
@@ -2,12 +2,21 @@ const { PORT } = require('./common/config');
 const app = require('./app');
 const { logger } = require('./lib/logger');
 
+// TODO:: remove after third cross-check
+// setTimeout(() => {
+//   throw new Error('Oops!');
+// }, 1500)
+//
+// setTimeout(() => {
+//   Promise.reject(new Error('Oops!'));
+// }, 1500)
+
 process
   .on('unhandledRejection', (reason, promise) => {
-    logger.info('Unhandled Rejection at Promise', reason, promise);
+    logger.error('Unhandled Rejection at Promise', reason, promise);
   })
   .on('uncaughtException', err => {
-    logger.info(`Uncaught Exception thrown: ${err.message}`, err);
+    logger.error(`Uncaught Exception thrown: ${err.message}`, err);
   });
 
 app.listen(PORT, () =>
