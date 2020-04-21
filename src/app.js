4e9525ab93f05461309a30231f203027bb8ca2c2
yurylapi <yuri.lapitsky@gmail.com>
2020-04-21 10:20:27 +0200

    Nodejs: Lithtml

--- src/app.js
+++ src/app.js
@@ -1,19 +1,23 @@
 const express = require('express');
 const morgan = require('morgan');
+
 const fs = require('fs');
 const {
   ErrorHandler,
   handleError,
   handleInternalError
 } = require('./lib/error-handler');
+
 const { reqLoggerMiddleware } = require('./lib/logger');
 const swaggerUI = require('swagger-ui-express');
 const path = require('path');
 const YAML = require('yamljs');
 const helmet = require('helmet');
+
 const userRouter = require('./resources/users/user.router');
 const boardRouter = require('./resources/boards/board.router');
 const taskRouter = require('./resources/tasks/task.router');
+
 const NodeInjectionMiddleware = require('node-dependency-injection-express-middleware')
   .default;
 const accessLogStream = fs.createWriteStream(
@@ -21,10 +25,18 @@ const accessLogStream = fs.createWriteStream(
   { flags: 'a' }
 );
 
-const app = express();
-const swaggerDocument = YAML.load(path.join(__dirname, '../doc/api.yaml'));
+const cookieParser = require('cookie-parser');
+const lessMiddleware = require('less-middleware');
+// const session = require('express-session');
 
+const app = express();
 app.use(helmet());
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+const swaggerDocument = YAML.load(path.join(__dirname, '../doc/api.yaml'));
 
 const options = {
   serviceFilePath: path.join(__dirname, './common/services.yaml'),
@@ -34,6 +46,10 @@ const options = {
 app.use(new NodeInjectionMiddleware(options).middleware());
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(lessMiddleware(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(
   morgan('combined', {
