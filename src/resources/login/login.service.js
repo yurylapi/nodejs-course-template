76b54cf4eb56b7f54910ef120d9d1503a48eb0b1
yurylapi <yuri.lapitsky@gmail.com>
2020-04-22 10:22:34 +0200

    Nodejs: Task 5
    - Authentication implementation

--- src/resources/login/login.service.js
+++ src/resources/login/login.service.js
@@ -16,10 +16,15 @@ class LoginService {
    */
   async getUserByLoginPassword(props) {
     const { login, password } = props;
-    const hashPassword = await this.bcrypt.hash(password, 8);
-    const user = this.userService.getUserByProps({ login, hashPassword });
+    const user = await this.userService.getUserByProps({ login });
 
     if (!user) {
+      throw new ErrorHandler(FORBIDDEN, getStatusText(FORBIDDEN));
+    }
+
+    const match = await this.bcrypt.compare(password, user.password);
+
+    if (!match) {
       throw new ErrorHandler(FORBIDDEN, getStatusText(FORBIDDEN));
     }
 
